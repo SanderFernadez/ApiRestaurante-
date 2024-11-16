92ad32eab2a21a2dd18a3940fcaf3cc6a7a8c999
Sander Fernandez <sanderfernandez0812@gmail.com>
2024-11-15 20:53:43 -0400

    Tables EndPoint

--- ApiRestaurante.WebApi/Controllers/v1/TablesController.cs
+++ ApiRestaurante.WebApi/Controllers/v1/TablesController.cs
@@ -1,5 +1,9 @@
 ﻿using ApiRestaurante.Core.Application.Interfaces.Services;
+using ApiRestaurante.Core.Application.Services;
+using ApiRestaurante.Core.Application.ViewModels.Plates;
 using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using RestauranteApi.Core.Application.ViewModels.Tables;
 
 
 namespace ApiRestaurante.WebApi.Controllers.v1
@@ -13,5 +17,109 @@ namespace ApiRestaurante.WebApi.Controllers.v1
         {
             _tableService = tableService;
         }
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveTableViewModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var tables = await _tableService.GetByIdSaveViewModel(id);
+
+                if (tables == null)
+                {
+                    return NoContent();
+                }
+
+                return Ok(tables);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post(SaveTableViewModel vm)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                await _tableService.Add(vm);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveTableViewModel))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Put(int id, SaveTableViewModel vm)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var plates = await _tableService.GetByIdSaveViewModel(id);
+
+                if (plates == null)
+                {
+                    return BadRequest();
+                }
+
+                await _tableService.Update(vm, id);
+                return Ok(vm);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SavePlateViewModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var Tables = await _tableService.GetAllListViewModel();
+
+                if (Tables == null || Tables.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(Tables);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+
+
     }
 }
